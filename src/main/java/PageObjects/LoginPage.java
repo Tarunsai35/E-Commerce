b8b965c9a54a6e03ef7d350a614f8f01043a22f1
Dinesh <dineshsai1746@gmail.com>
2024-12-19 23:49:34 +0530

    login

--- src/main/java/PageObjects/LoginPage.java
+++ src/main/java/PageObjects/LoginPage.java
@@ -1,5 +1,37 @@
 package PageObjects;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class LoginPage {
+    WebDriver driver;
+
+    // Locators
+    private By usernameField = By.id("email");
+    private By passwordField = By.id("pass");
+    private By loginButton = By.xpath("//button[@data-testid='royal_login_button']");
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Actions
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
 }
--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -1,5 +1,27 @@
+// Tests/LoginTest.java
 package Tests;
 
-public class LoginTest {
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import PageObjects.LoginPage;
 
+public class LoginTest {
+    WebDriver driver;
+
+    @Test
+    public void testLogin() throws InterruptedException {
+      //  System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("9666916895", "7997402963");
+        Thread.sleep(5000);
+        loginPage.clickLogin();
+
+        // Assert page title or successful login indicator
+        assert driver.getTitle().contains("Facebook – log in or sign up");
+        driver.quit();
+    }
 }
